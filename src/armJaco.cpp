61e1e70c11b5ffea957b67cc560069ff1ef8fab3
Varun-Haris <47725022+Varun-Haris@users.noreply.github.com>
2019-11-18 00:40:53 -0500

    Update armJaco.cpp

--- src/armJaco.cpp
+++ src/armJaco.cpp
@@ -2,8 +2,14 @@
 #include <algorithm>
 #include <math.h>
 #include <vector>
+
 #include <ros/ros.h>
+#include <cv_bridge/cv_bridge.h>
 #include <geometry_msgs/PoseStamped.h>
+#include <geometry_msgs/Vector3.h>
+#include <sensor_msgs/PointCloud2.h>
+#include <sensor_msgs/PointCloud.h>
+#include <sensor_msgs/point_cloud_conversion.h>
 #include "kinova_driver/kinova_api.h"
 #include "kinova_driver/kinova_arm.h"
 #include "kinova_driver/kinova_tool_pose_action.h"
@@ -13,7 +19,12 @@
 
 #include <actionlib/client/simple_action_client.h>
 
+// ZED SDK
+#include <sl/Camera.hpp>
+
 using namespace std;
+using namespace cv;
+using namespace sl;
 
 // Setting up the actionlib
 typedef actionlib::SimpleActionClient<kinova_msgs::ArmPoseAction> armPose;
@@ -22,20 +33,47 @@ typedef actionlib::SimpleActionClient<kinova_msgs::SetFingersPositionAction> fin
 bool homeSet = false;
 bool waypointSet = false;
 const double FINGER_MAX = 6400;
+const int width = 640;
+const int height = 360;
 
 class armJaco{
 public:
 	ros::NodeHandle nh;
-	ros::Subscriber sub;
-	geometry_msgs::PoseStamped currentPose;
+	ros::Subscriber sub, seg, pc_sub;
+	// The next pose is the pre-grasp pose (0.2 m above the detected object)
+	geometry_msgs::PoseStamped home, currentPose, nextPose, dropPose;
+	geometry_msgs::Vector3 c[width][height];
+	sensor_msgs::PointCloud point_cloud;
+	int isObject[width][height],t[width][height];
+	float mean_x = 0.0, mean_y = 0.0, mean_z = 0.0;
+	cv_bridge::CvImagePtr cvPtr;
 
 	armJaco():nh(){
-		sub = nh.subscribe("/j2n6s300_driver/out/tool_pose",1,&armJaco::getArmPose,this);
+		sub = nh.subscribe("/j2n6s300_driver/out/tool_pose",1,&armJaco::currentPoseFeedback,this);
+		seg = nh.subscribe("/enet/seg",1,&armJaco::getSegmentedImage,this);
+		pc_sub = nh.subscribe("/enet/point_cloud",1,&armJaco::getPointCloud,this);
 		ROS_INFO_STREAM("Setup successful !!");
 	}
 
-	void getArmPose(const geometry_msgs::PoseStamped& pose){
+	// Pose callback
+	void currentPoseFeedback(const geometry_msgs::PoseStamped& pose){
 	    currentPose = pose;
+
+			if (homeSet == false){
+				home = currentPose;
+				ROS_INFO_STREAM("Home position set !!");
+				homeSet = true;
+			}
+	}
+
+	// Segmented image callback
+	void getSegmentedImage(const sensor_msgs::ImageConstPtr& seg_img){
+		cvPtr = cv_bridge::toCvCopy(seg_img, sensor_msgs::image_encodings::BGR8);
+	}
+
+	// Point cloud callback
+	void getPointCloud(const sensor_msgs::PointCloud2& cloud){
+		sensor_msgs::convertPointCloud2ToPointCloud(cloud, point_cloud);
 	}
 
 	void sendArmPoseGoal(geometry_msgs::PoseStamped &goal_pose){
@@ -63,15 +101,173 @@ public:
 		client.sendGoal(goal);
 	}
 
-	void goHome(geometry_msgs::PoseStamped home){
+	void goHome(){
 		home.header.stamp = ros::Time::now();
 		home.header.frame_id = "j2n6s300_link_base";
-		
+
 		this->sendArmPoseGoal(home);
 		ROS_INFO_STREAM("Back home");
 	}
 
-};
+	void seg2obj(){
+		cv::Mat frame = cvPtr->image;
+		int sum = 0;
+		for(int i=0; i<frame.rows; i++){
+			for(int j=0; j<frame.cols; j++){
+				int g = frame.at<Vec3b>(i,j)[1];
+
+				if(g>35) t[i][j] = 0;
+				else t[i][j] = 1;
+			}
+		}
+	//cout << sum << endl;
+	}
+
+	void computeMidPoint(){
+		seg2obj();
+
+		double sum_x = 0, sum_y = 0, sum_z = 0, cx = 0, cy = 0, cz = 0;
+		int object_point_count = 0;
+		for (int i = 0; i < width; i++){
+			for (int j = 0; j < height; j++){
+				isObject[i][j] = t[i][j];
+
+				if (!(isnan(point_cloud.points[i*j].x) || isinf(point_cloud.points[i*j].x))) cx = double(point_cloud.points[i*j].x);
+				if (!(isnan(point_cloud.points[i*j].y) || isinf(point_cloud.points[i*j].y))) cy = double(point_cloud.points[i*j].y);
+				if (!(isnan(point_cloud.points[i*j].z) || isinf(point_cloud.points[i*j].z))) cz = double(point_cloud.points[i*j].z);
+
+				//cout << point_cloud.points[i*j].x << " " << point_cloud.points[i*j].y << " " << point_cloud.points[i*j].z << endl;
+				c[i][j].x = cx;
+				c[i][j].y = cy;
+				c[i][j].z = cz;
+
+				if (isObject[i][j] && c[i][j].z < 0) {
+					sum_x += i;
+					sum_y += j;
+					object_point_count++;
+				}
+			}
+		}
+		// calculate min and max in each direction
+		//int object_point_count_1 = 0;
+		double max_xdir[2] ;
+		double min_xdir[2] ;
+		double max_ydir[2] ;
+		double min_ydir[2] ;
+		max_xdir[0] = 0;
+		max_xdir[1] = 0;
+		min_xdir[0] = width;
+		min_xdir[1] = width;
+		max_ydir[0] = 0;
+		max_ydir[1] = 0;
+		min_ydir[0] = height;
+		min_ydir[1] = height;
+		for (int i = 0; i < width; i++){
+			for (int j = 0; j < height; j++) {
+				if (isObject[i][j] && c[i][j].z < 0) {
+					//sum_x += i;
+
+					if(i>max_xdir[0]){
+						max_xdir[0]=i;
+						max_xdir[1]=j;
+					}
+					if(i<min_xdir[0]){
+						min_xdir[0]=i;
+						min_xdir[1]=j;
+					}
+					if(j>max_ydir[1]){
+						max_ydir[0]=i;
+						max_ydir[1]=j;
+					}
+					if(j<min_ydir[1]){
+						min_ydir[0]=i;
+						min_ydir[1]=j;
+					}
+					//object_point_count++;
+				}
+			}
+		}
+		max_xdir[1] = -1*max_xdir[1];
+
+		min_xdir[1] = -1*min_xdir[1];
+
+		max_ydir[1] = -1*max_ydir[1];
+
+		min_ydir[1] = -1*min_ydir[1];
+
+
+		double d1=0, d2=0;
+		double alpha = 0;
+		d1 = sqrt((max_xdir[0]-max_ydir[0])*(max_xdir[0]-max_ydir[0])+(max_xdir[1]-max_ydir[1])*(max_xdir[1]-max_ydir[1]));
+		d2 = sqrt((min_xdir[0]-max_ydir[0])*(min_xdir[0]-max_ydir[0])+(min_xdir[1]-max_ydir[1])*(min_xdir[1]-max_ydir[1]));
+		alpha = atan((max_xdir[1]-max_ydir[1])/(max_xdir[0]-max_ydir[0]));
+		alpha = (alpha*180/3.14);
+		if(d2>d1){
+			alpha = (alpha)+90;
+		}
+
+		// Mid point in the pixel domain
+		int pixel_x = int(round(sum_x / object_point_count));
+		int pixel_y = int(round(sum_y / object_point_count));
+		//cout << pixel_x << " " << pixel_y << endl;
+
+		// Extract the actual from the point cloud ==> With respect to camera
+		if (abs(pixel_x) < width && abs(pixel_y) < height){
+			mean_x = c[pixel_x][pixel_y].x;
+			mean_y = c[pixel_x][pixel_y].y;
+			mean_z = c[pixel_x][pixel_y].z;
+		}
+
+		// Generate next waypoint based on frame transformation matrices
+		generateNextWaypoint();
+	}	// Compute mid point function
+
+	void generateNextWaypoint(){
+		nextPose.header.stamp = ros::Time::now();
+		nextPose.header.frame_id = "j2n6s300_link_base";
+
+		// Trasformation from the camera frame to the hand frame
+		float mean_x_pose, mean_y_pose, mean_z_pose;
+		float delta_x=0, delta_y=350, delta_z=780;
+    mean_x_pose = -(mean_x)/1000+0.08;
+    mean_y_pose = (mean_y + 200)/1000 + 0.06;
+    mean_z_pose = (delta_z - mean_z) /1000 - 0.08;
+
+		nextPose.pose.position.x = mean_x_pose;
+		nextPose.pose.position.y = mean_y_pose;
+		nextPose.pose.position.z = mean_z_pose + 0.2;
+
+		nextPose.pose.orientation = home.pose.orientation;
+	}
+
+	void grasp(ros::Rate loop_rate){
+		// Go to pre-grasp pose
+		sendArmPoseGoal(nextPose);
+		loop_rate.sleep();
+
+		// Go to grasp pose
+		nextPose.pose.position.z = nextPose.pose.position.z - 0.2;
+		sendArmPoseGoal(nextPose);
+		loop_rate.sleep();
+
+		// Pick up the object
+		sendFingerPoseGoal(55);
+		loop_rate.sleep();
+
+		// Move to drop pose
+		dropPose = currentPose;
+		dropPose.pose.position.y = dropPose.pose.position.y + 0.5;
+		loop_rate.sleep();
+
+		// Drop the object
+		sendFingerPoseGoal(0);
+		loop_rate.sleep();
+
+		// Return home
+		goHome();
+		nextPose = home;
+	}
+}; // End of class
 
 int main(int argc, char** argv){
 	ros::init(argc, argv, "Jaco");
@@ -85,27 +281,15 @@ int main(int argc, char** argv){
 
 	while(true){
 		ros::spinOnce();
-		if (homeSet == false){
-			home = arm->currentPose;
-			ROS_INFO_STREAM("Home position set !!");
-			homeSet = true;
-			continue;
-		}
 
-		goal = home;
-		// Go to a defined cartesian waypoint
-		goal.pose.position.x = goal.pose.position.x + 0.3;
-		arm->sendArmPoseGoal(goal);
+		arm->computeMidPoint();
+		cout << "Next waypoint  is: " << arm->nextPose << endl;
 		loop_rate.sleep();
-		// Close all the fingers (Equivalent to object grasping)
-		arm->sendFingerPoseGoal(float(55));
-		loop_rate.sleep();
-		// Open all the fingers
-		arm->sendFingerPoseGoal(float(0));
-		loop_rate.sleep();
-		// Return to home position after the task execution
-		arm->goHome(home);
-		break;
-    }
+		//
+		// Put an if condition to define grasping scenario ==> Constraints when arm can go and grab the object
+		//
+		//arm->grasp();
+		//break;
+  }
 	return 0;
 }
